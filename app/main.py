b296d3f6a7b5b59d2a53e2c3726827870e80ab7e
alex aguirre <alex.aguirre@culqi.com>
2019-12-03 17:29:00 -0500

    deploy en gitlab

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Python!"
+@app.route("/", methods=['POST'])
+def requestPrint():
+    headersOut = request.headers.to_list()
+    response = {
+        "headers" : headersOut,
+        "body" : request.json
+    } 
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
